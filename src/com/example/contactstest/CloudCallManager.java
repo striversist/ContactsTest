707270dba385cf5c8d7a9fb4c4b18d9b1f5793b6
strieversist <striversist@qq.com>
2014-05-13 10:38:55 +0800

    【功能】增加“挂断电话”的功能

--- src/com/example/contactstest/CloudCallManager.java
+++ src/com/example/contactstest/CloudCallManager.java
@@ -1,9 +1,15 @@
 package com.example.contactstest;
 
+import java.lang.reflect.Method;
+
+import com.android.internal.telephony.ITelephony;
+
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -36,6 +42,14 @@ public class CloudCallManager {
                 }
             }
         });
+        call("18602769673");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                boolean success = endCall();
+                Log.d(TAG, "endCall success=" + success);
+            }
+        }, 10000);
     }
 
     public void registerCallStateListener(PhoneStateListener listener) {
@@ -62,5 +76,21 @@ public class CloudCallManager {
                 + number));
         dialIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         mContext.startActivity(dialIntent);
+    }
+    
+    /**
+     * 挂断电话
+     * @return 是否挂断成功
+     */
+    public boolean endCall() {
+        try {
+            Method method = Class.forName("android.os.ServiceManager").getMethod("getService", String.class);
+            IBinder binder = (IBinder) method.invoke(null, new Object[]{"phone"});
+            ITelephony telephony = ITelephony.Stub.asInterface(binder);
+            return telephony.endCall();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
